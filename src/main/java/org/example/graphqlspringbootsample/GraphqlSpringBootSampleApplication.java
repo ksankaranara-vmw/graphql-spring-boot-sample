0bbbc0e28dcb7358d440a635bded2782a5dd60c4
ksankaranara <karthik.sankaranarayanan@broadcom.com>
2024-12-28 21:22:14 +0530

    Add scaffolding

---
+++ src/main/java/org/example/graphqlspringbootsample/GraphqlSpringBootSampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.graphqlspringbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSpringBootSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/graphqlspringbootsample/GraphqlSpringBootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.graphqlspringbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSpringBootSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
